83ee1af96b503d3a00ec694d8dcaa592b4933436
Liliana <liliana.nu@gmail.com>
2018-02-01 13:57:31 +0100

    Adding some initial methods for the baseball division

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -1,50 +1,136 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
 
+  Map<String, Integer> teams;
+  int[] wins;
+  int[] looses;
+  int[] remaining;
+  int[][] games;
+  boolean[] isEliminated;
+  // Not sure about this
+  List<String> certificates;
+
   // create a baseball division from given filename in format specified below
   public BaseballElimination(String filename) {
+    if (filename == null) {
+      throw new IllegalArgumentException("Filename is null");
+    }
+
+    In in = new In(filename);
+    int amountTeams = in.readInt();
+    // Initialize variables
+    teams = new HashMap<>();
+    wins = new int[amountTeams];
+    looses = new int[amountTeams];
+    remaining = new int[amountTeams];
+    games = new int[amountTeams][amountTeams];
+
+    for (int i = 0; i < amountTeams; i++) {
+      teams.put(in.readString(), i);
+      wins[i] = in.readInt();
+      looses[i] = in.readInt();
+      remaining[i] = in.readInt();
+      for (int j = 0; j < amountTeams; j++) {
+        games[i][j] = in.readInt();
+      }
+    }
+    isEliminated = new boolean[amountTeams];
+    certificates = new ArrayList<>();
+
+    trivialElimination();
+    nonTrivialElimination();
+
+  }
+
+  private void nonTrivialElimination() {
+    // TODO Auto-generated method stub
+
+  }
+
+  private void trivialElimination() {
+    // TODO Auto-generated method stub
 
   }
 
   // number of teams
   public int numberOfTeams() {
-    return 0;
+    return teams.size();
   }
 
   // all teams
   public Iterable<String> teams() {
-    return null;
+    return new ArrayList<>(teams.keySet());
   }
 
   // number of wins for given team
   public int wins(String team) {
+    if (team == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return 0;
   }
 
   // number of losses for given team
   public int losses(String team) {
+    if (team == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return 0;
   }
 
   // number of remaining games for given team
   public int remaining(String team) {
+    if (team == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return 0;
   }
 
   // number of remaining games between team1 and team2
   public int against(String team1, String team2) {
+    if (team1 == null || team2 == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return 0;
   }
 
   // is given team eliminated?
   public boolean isEliminated(String team) {
+    if (team == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return false;
   }
 
   // subset R of teams that eliminates given team; null if not eliminated
   public Iterable<String> certificateOfElimination(String team) {
+    if (team == null) {
+      throw new IllegalArgumentException("Team arg cannot be null");
+    }
     return null;
+  }
+
+  public static void main(String[] args) {
+    BaseballElimination division = new BaseballElimination(args[0]);
+    for (String team : division.teams()) {
+      StdOut.print(team + " ");
+      // if (division.isEliminated(team)) {
+      // StdOut.print(team + " is eliminated by the subset R = { ");
+      // for (String t : division.certificateOfElimination(team)) {
+      // StdOut.print(t + " ");
+      // }
+      // StdOut.println("}");
+      // } else {
+      // StdOut.println(team + " is not eliminated");
+      // }
+    }
   }
 
 }
--- tests/BaseballEliminationTest.java
+++ tests/BaseballEliminationTest.java
@@ -1,12 +1,97 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
 
 import org.junit.Test;
 
 public class BaseballEliminationTest {
 
+  @Test(expected = IllegalArgumentException.class)
+  public void testConstructorArgumentIsNull() {
+    new BaseballElimination(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testWinsWithNullArgument() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.wins(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testLossesWithNullArgument() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.losses(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testRemainingWithNullArgument() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.remaining(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIsEliminatedWithNullArgument() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.isEliminated(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCertificateOEWithNullArgument() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.certificateOfElimination(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testAgainstWithNullArgment() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams1.txt");
+    be.against(null, "Any Name");
+  }
+
+  // Checks creates and reads structures correctly
+
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testTeamsAndDataIsLoaded() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams10.txt");
+
+    assertEquals(10, be.teams.length);
+    assertEquals(10, be.wins.length);
+    assertEquals(10, be.looses.length);
+    assertEquals(10, be.remaining.length);
+
+    // Actually check on some data
+    assertEquals(63, be.remaining[0]);
+    assertEquals(31, be.wins[8]);
+    assertEquals(31, be.looses[9]);
+    assertEquals("Boston", be.teams[1]);
+
+    // Assert some of the games data
+    // Boston - Indiana 0
+    assertEquals(0, be.games[1][9]);
+    // Denver - Atlanta 9
+    assertEquals(9, be.games[5][0]);
+  }
+
+  // How do I test inmutability here?
+  // @Test
+  // public void testInmutableTeams() throws Exception {
+  // BaseballElimination be = new BaseballElimination("data/teams10.txt");
+  //
+  // Iterable<String> te = be.teams();
+  //
+  // assertNotNull(be.teams()); te = null;
+  // }
+
+  @Test
+  public void testTeamsAre10() throws Exception {
+    BaseballElimination be = new BaseballElimination("data/teams10.txt");
+
+    int count = 0;
+    Iterator<String> it = be.teams().iterator();
+    while (it.hasNext()) {
+      count++;
+      it.next();
+    }
+    assertEquals(10, count);
   }
 
 }
